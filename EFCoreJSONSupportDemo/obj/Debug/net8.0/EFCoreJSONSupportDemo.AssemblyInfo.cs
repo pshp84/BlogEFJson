9d4ba7033c4cdc60d0671766f9540dfb646982ff
Hiren <ps28new.developer@outlook.com>
2025-05-21 21:02:13 +0530

    Extra code removal

--- EFCoreJSONSupportDemo/obj/Debug/net8.0/EFCoreJSONSupportDemo.AssemblyInfo.cs
+++ EFCoreJSONSupportDemo/obj/Debug/net8.0/EFCoreJSONSupportDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreJSONSupportDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25489d8057230b5bcbbae8551128e3e557f1773")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreJSONSupportDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreJSONSupportDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
